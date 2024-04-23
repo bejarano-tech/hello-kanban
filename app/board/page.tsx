02e6b2998366e1d2199cd4265347919a59fe725b
Frederick Bejarano Sanchez <bejarano.frederick@gmail.com>
2024-04-23 15:30:46 -0500

    fix gitignore add board page and add colum prisma schema

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -1,9 +1,10 @@
 import { BoardComponent } from "@/components/board/board-component";
 import { getTasks } from "@/data/task";
+import { Task } from "@prisma/client";
 
 export default async function Board() {
   const tasks = await getTasks()
   return (
-    <BoardComponent tasks={tasks}/>
+    <BoardComponent tasks={tasks as Task[]}/>
   );
 }
