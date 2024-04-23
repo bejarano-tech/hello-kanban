038a32cc2ab411abd6ac3b3d17eb9a9fad29baa3
Frederick Bejarano Sanchez <bejarano.frederick@gmail.com>
2024-04-23 18:07:22 -0500

    drag and drop second attemp

--- components/board/board-component.tsx
+++ components/board/board-component.tsx
@@ -1,56 +1,41 @@
 "use client";
-import { DndContext, DragEndEvent, UniqueIdentifier } from "@dnd-kit/core";
-import { Draggable } from "./draggable";
-import { Droppable } from "./droppable";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { ColumnWithTasks } from "@/data/column";
+import { Task } from "@prisma/client";
+import { DndContext, PointerSensor, useSensor, useSensors } from "@dnd-kit/core";
 
 interface BoardComponent {
-  columns: ColumnWithTasks[];
+  columns: ColumnWithTasks[]
+  tasks: Task[]
 }
 
-export const BoardComponent = ({ columns }: BoardComponent) => {
-  const [parent, setParent] = useState<UniqueIdentifier | null>(null);
-  const containers = ['A', 'B', 'C'];
+export const BoardComponent = ({ columns: defaultColums, tasks: defaultTasks }: BoardComponent) => {
+  const [columns, setColumns] = useState<ColumnWithTasks[]>(defaultColums);
+  const columnsId = useMemo(() => columns.map((col) => col.id), [columns]);
 
-  const draggableMarkup = <Draggable id="1">Drag me</Draggable>;
+  const [tasks, setTasks] = useState<Task[]>(defaultTasks);
 
-  const handleDragEnd = (event: DragEndEvent) => {
-    const { over } = event;
+  const [activeColumn, setActiveColumn] = useState<ColumnWithTasks | null>(null);
 
-    // If the item is dropped over a container, set it as the parent
-    // otherwise reset the parent to `null`
-    setParent(over ? over.id : null);
-  };
+  const [activeTask, setActiveTask] = useState<Task | null>(null);
 
-  useEffect(() => {
-    console.log(parent)
-  }, [parent])
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 10,
+      },
+    })
+  );
 
   return (
     <div className="flex flex-col justify-center p-8">
       <h1 className="text-4xl font-bold text-center mb-8">Hellow Kanban</h1>
-      <DndContext onDragEnd={handleDragEnd}>
-        {parent === null ? draggableMarkup : null}
-        <div>
         <div className="flex gap-4 p-4 justify-center">
-          {containers.map((column) => (
-            <Droppable key={column} id={column}>
-              <div className="bg-gray-200 p-4 rounded w-64">
-                <h2 className="font-bold mb-2">{column}</h2>
-                {parent === column ? draggableMarkup : "Drop here"}
-              </div>
-            </Droppable>
-          ))}
+        <DndContext
+          sensors={sensors}
+        >
+          </DndContext>
         </div>
-        </div>
-      </DndContext>
-      {/* <DndContext onDragEnd={handleDragEnd}>
-        {!isDropped ? draggableMarkup : null}
-        <div className="flex gap-4 p-4 justify-center">
-          <Droppable>{isDropped ? draggableMarkup : "Drop here"}</Droppable>
-        </div>
-      </DndContext> */}
     </div>
   );
 };
